6e55b2e618e9394e622d95972d2938a895ac2a7c
poonamgit <poonam_pal@persistent.com>
2021-06-03 14:48:15 +0530

    Added Function Headers

--- math.py
+++ math.py
@@ -1,11 +1,15 @@
+#Addition Header
 def add(x,y):
     pass
 
+#Subtraction Header
 def sub(x,y):
     pass
 
+#Multiplication Header
 def mul(x,y):
     pass
 
+#Division Header
 def div(x,y):
     pass
